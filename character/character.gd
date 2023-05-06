f4b419040279269c3d0a284969e5e4537373f2a9
OliverOA <OliverAOakman@gmail.com>
2023-05-06 19:47:44 +1000

    Add itch project and butler pipeline

--- character/character.gd
+++ character/character.gd
@@ -2,7 +2,7 @@ class_name Astronaut extends RigidBody2D
 
 @export var thrust_ratio: float = 1.0
 @export var thrust_power: float = 5.0
-@export var max_speed: float = 500.0
+@export var max_speed: float = 700.0
 
 @export var rotation_strength: float = 200.0
 @export var max_rotation_speed: float = 4.0
@@ -21,11 +21,11 @@ var debug_text: String = ""
 var rotating_clockwise: bool = false
 var rotating_anticlockwise: bool = false
 
-@export var latch_strength: float = 0.1
+@export var latch_strength: float = 0.025
 var target_latch: Vector2
 var latching_area: Area2D
 var latching_distance: float = 100.0
-var latch_distance_threshold: float = 20.0
+var latch_distance_threshold: float = 25.0
 
 var state = State.LATCHED
 
@@ -44,16 +44,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	debug_text = "linear velocity: {lin_vel}, angular velocity: {ang_vel}, state: {state}, latching_distance: {latching_distance}"
 	debug_label.text = debug_text.format({"lin_vel": linear_velocity.length(), "ang_vel": angular_velocity, "state": state, "latching_distance": latching_distance})
-
-
-#func _integrate_forces(physics_state: PhysicsDirectBodyState2D):
-#	if state == State.LATCHING:
-#		global_position = lerp(global_position, target_latch, latch_strength)
-#		# Check if close enough
-#		if latching_distance.length() < latch_distance_threshold:
-#			print("LATCHED")
-#			state = State.LATCHED
-#			latching_distance = Vector2(100.0, 100.0)
 
 
 func _physics_process(delta: float) -> void:
